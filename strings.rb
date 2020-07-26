2f6629774a76e37ea2309fcc4fcc4622c6c87e3b
rana-abhi777 <abhisekrana777@gmail.com>
2020-07-26 22:11:59 +0530

    strings - concatenation, append and some built-in functions

---
+++ strings.rb
@@ -0,0 +1,29 @@
+# concatenate strings
+msg = 'Hello! '
+name = 'John'
+puts msg + ' ' + name
+
+# another way is to use append
+# append using "<<"
+hello_msg = "Hello"
+hello_msg << " World"
+puts hello_msg
+
+puts msg * 3
+
+# some built in methods
+str_test = "jOhn dOe"
+
+puts "Original string: " + str_test
+
+# reverse
+puts str_test.reverse
+
+# upcase
+puts str_test.upcase
+
+# downcase
+puts str_test.downcase
+
+# captilize
+puts str_test.capitalize
