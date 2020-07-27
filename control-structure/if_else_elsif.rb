9fe105f208f0802a41ed2ff8c6d5b129db58cfef
rana-abhi777 <abhisekrana777@gmail.com>
2020-07-27 23:40:11 +0530

    Learned if elsif else statements

---
+++ control-structure/if_else_elsif.rb
@@ -0,0 +1,15 @@
+# if block
+fruit = 'apple'
+if fruit == 'apple'
+  puts fruit.upcase
+end
+
+# if elsif block
+x = 15
+if x < 10
+  puts "Less than 10"
+elsif x >=20
+  puts "Greater than or equal to 20"
+else
+  puts "Between 10 and 20"
+end
