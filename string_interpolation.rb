fa329ff3e193c41b762574712964efa6950e449b
rana-abhi777 <abhisekrana777@gmail.com>
2020-07-27 10:24:48 +0530

    Learned string interpolation and variables in ruby

---
+++ string_interpolation.rb
@@ -0,0 +1,5 @@
+first_name = 'John'
+last_name = 'Doe'
+age = 45
+
+puts "Details are =\tName: #{first_name} #{last_name}, age: #{age}"
---
+++ variables.rb
@@ -0,0 +1,7 @@
+# ruby convention to name variables
+# lowercase with underscores
+x = 10
+first_name = "John Doe"
+
+puts x
+puts first_name
