a7e2c512c9a40409925a563aabd8260305ba1b65
rana-abhi777 <abhisekrana777@gmail.com>
2020-07-27 13:18:18 +0530

    learned booleans and comparison operators

---
+++ booleans.rb
@@ -0,0 +1,13 @@
+is_true = true
+is_false = false
+
+puts is_true
+puts is_true.class
+
+puts is_false
+puts is_false.class
+
+# comparison operators as well
+x = 1
+puts "#{x<3 && x > 100}"
+puts "#{x<3 || x < 100}"
