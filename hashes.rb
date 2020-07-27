dc60ec721718f990e03d55da407fda242f0f7b8f
rana-abhi777 <abhisekrana777@gmail.com>
2020-07-27 10:49:59 +0530

    hashed in ruby - key-value pairs

---
+++ hashes.rb
@@ -0,0 +1,12 @@
+# hashes are key-value pairs
+# just like dictionaries in python
+car = {
+  'brand' => 'Ford',
+  'model' => 'Mustang',
+  'color' => 'blue',
+  'interior_color' => 'tan'
+}
+
+puts "Our hash for car is :"
+puts "#{car}"
+puts "#{car['brand']}"
