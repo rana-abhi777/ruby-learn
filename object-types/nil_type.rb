098d106e0e7e6dac84482bd34b83bb616c8b8a6a
rana-abhi777 <abhisekrana777@gmail.com>
2020-07-27 17:24:49 +0530

    Learned nil object type

---
+++ object-types/nil_type.rb
@@ -0,0 +1,12 @@
+# nil is an object in ruby
+puts nil.class
+
+# and nil is not equal to false
+puts nil==false
+
+# check if a variable is nil or not
+product = nil
+
+puts product.nil?
+puts product == nil
+puts !product
