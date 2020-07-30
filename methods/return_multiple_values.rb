d1cbb6d45afa0c05de6802e8c47dc96feb3a66b2
rana-abhi777 <abhisekrana777@gmail.com>
2020-07-31 00:03:55 +0530

    return multiple values with hash and array

--- methods/return_multiple_values.rb
+++ methods/return_multiple_values.rb
@@ -1,9 +1,22 @@
-# in ruby we can return multiple values with hash
+# in ruby we can return multiple values with hash or array
+
+# using hash
 def add_and_subtract(n1,n2)
   add = n1 + n2
-  sub = n1 - n1
+  sub = n1 - n2
   {:add => add, :sub => sub}
 end
 
 result = add_and_subtract(10,2)
 puts "For (10,2), Addtition is #{result[:add]}, and subtraction is : #{result[:sub]}"
+
+
+# using array
+def add_and_subtract(n1,n2)
+  add = n1 + n2
+  sub = n1 - n2
+  [add, sub]
+end
+
+add, sub = add_and_subtract(99,65)
+puts "For (99,65), additions is : #{add}, and subtraction is : #{sub}"
