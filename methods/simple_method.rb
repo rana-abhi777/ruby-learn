787a89cffe7e8d1fa433e02dbe48c4609d536f1e
rana-abhi777 <abhisekrana777@gmail.com>
2020-07-30 23:04:00 +0530

    methods in ruby

---
+++ methods/simple_method.rb
@@ -0,0 +1,15 @@
+# in ruby since everything is object oriented
+# so functions are called methods
+
+def print_name
+  puts "Hello world from method!"
+end
+
+print_name
+
+# can be redefined again
+def print_name
+  puts "Hello world from method again!"
+end
+
+print_name
